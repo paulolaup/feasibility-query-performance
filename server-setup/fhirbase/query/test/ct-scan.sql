bb63a28a7b8ddee34a23ebf0c32962f8fb12e289
paulolaup <paul-behrend@outlook.de>
2023-12-14 00:04:52 +0100

    Add initial queries for fhirbase.

--- server-setup/fhirbase/query/test/ct-scan.sql
+++ server-setup/fhirbase/query/test/ct-scan.sql
@@ -1,4 +1,4 @@
 SELECT COUNT(*) FROM patient p
     JOIN "procedure" proc ON proc.resource #>> '{subject,id}' = p.id
-    WHERE (proc.resource -> 'code' -> 'coding' <@ 'subsumedBy(http://snomed.info/sct|77477000)')
+    WHERE (proc.resource -> 'code' -> 'coding' @> 'subsumedBy(http://snomed.info/sct|77477000)')
 ;
--- server-setup/fhirbase/query/test/diabetes-mellitus.sql
+++ server-setup/fhirbase/query/test/diabetes-mellitus.sql
@@ -1,4 +1,4 @@
 SELECT COUNT(DISTINCT(p.id)) FROM patient p
     JOIN condition c ON c.resource #>> '{subject,id}' = p.id
-    WHERE (c.resource -> 'code' -> 'coding' <@ 'subsumedBy(http://snomed.info/sct|73211009)')
+    WHERE (c.resource -> 'code' -> 'coding' @> 'subsumedBy(http://snomed.info/sct|73211009)')
 ;
