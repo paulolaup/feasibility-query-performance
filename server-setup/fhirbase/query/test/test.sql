efff2ac0569e38738b3b8e6ac330371d0cb4faeb
paulolaup <paul-behrend@outlook.de>
2024-01-08 15:38:25 +0100

    Commit before merge

--- server-setup/fhirbase/query/test/test.sql
+++
@@ -1,4 +0,0 @@
-SELECT COUNT(DISTINCT(p.id)) FROM patient p
-    JOIN condition d ON d.resource #>> '{subject,id}' = p.id
-    WHERE (d.resource @> '{"code": {"coding": [{"system": "http://snomed.info/sct", "code": "72892002"}]}}')
-;
