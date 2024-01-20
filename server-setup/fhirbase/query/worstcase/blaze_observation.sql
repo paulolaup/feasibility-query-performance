ad914c3b44ed01d6a26b1a4217a4b4b79e7bb0cd
Confi Dential <confi@localhost.localdomain>
2024-01-20 14:49:57 +0100

    Add worst case queries for Pathling

--- server-setup/fhirbase/query/worstcase/blaze_observation.sql
+++ server-setup/fhirbase/query/worstcase/blaze_observation.sql
@@ -3,6 +3,5 @@ FROM (SELECT o.resource #>> '{subject,id}' AS id
        FROM observation o,
             jsonb_array_elements(o.resource #> '{code,coding}') coding
        WHERE coding ->> 'system' = 'http://loinc.org'
-         AND o.resource #> '{value,Quantity}' ->> 'system' = 'http://unitsofmeasure.org'
          AND coding ->> 'code' = '753-4') inclusion
 ;
