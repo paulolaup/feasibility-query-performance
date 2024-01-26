4d76bf00b997c192d1704ff097312a95725ad2c2
Confi Dential <confi@localhost.localdomain>
2024-01-26 13:36:16 +0100

    Remove Pathling queries with long code disjuntions due to long execution times

--- server-setup/pathling/query/test/bee-venom.sql
+++
@@ -1,6 +0,0 @@
-SELECT COUNT(DISTINCT(inclusion.id))
-FROM (SELECT t.resource #>> '{patient,id}' AS id
-FROM allergyintolerance t,
-     jsonb_array_elements(t.resource #> '{code,coding}') coding
-WHERE coding ->> 'system' = 'http://snomed.info/sct'and coding ->> 'code' = '288328004') AS inclusion
-;
